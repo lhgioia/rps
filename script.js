c46993ddf6f347af5a18aebda383b1a8092636d2
lhgioia <lhgioia@gmail.com>
2022-11-28 19:14:36 -0800

    Added function to return rock paper or scissors based on random number

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // We makin games
+
+let getComputerChoice = function() {
+    let randomNumber = Math.random();
+    if (randomNumber < 0.33) return "rock";
+    if (randomNumber >= 0.33 && randomNumber <= 0.66) return "paper";
+    if (randomNumber > 0.66) return "scissors";
+}
+
